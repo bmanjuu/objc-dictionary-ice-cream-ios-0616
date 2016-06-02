68328f03ad068ef8d62b9ac6f4a70ad2952e0f9d
bmanjuu <bettyofung@gmail.com>
2016-06-02 17:25:23 -0400

    third pass at ice cream lab because we all scream for ice cream

--- objc-dictionary-ice-cream/FISAppDelegate.m
+++ objc-dictionary-ice-cream/FISAppDelegate.m
@@ -10,10 +10,40 @@
     return YES;
 }
 
-/**
- 
- * Write your custom method bodies here.
- 
- */
+-(NSArray *)namesForIceCream:(NSString *)iceCream{
+    
+    NSDictionary *iceCreamRequests = @{ @"Joe" : @"Peanut Butter and Chocolate",
+                                        @"Tim" : @"Natural Vanilla",
+                                        @"Sophie" : @"Mexican Chocolate",
+                                        @"Deniz" : @"Natural Vanilla",
+                                        @"Tom" : @"Mexican Chocolate",
+                                        @"Jim" : @"Natural Vanilla",
+                                        @"Mark" : @"Cookies 'n Cream" };
+    
+    NSMutableArray *peopleForIceCream = [[NSMutableArray alloc] init];
+    
+    for(NSString *name in iceCreamRequests){
+        if([[iceCreamRequests valueForKey:name] isEqualToString:iceCream]){
+            [peopleForIceCream addObject:name];
+        }
+    }
+    
+    return peopleForIceCream;
+}
+
+
+
+-(NSDictionary *)countsOfIceCream:(NSDictionary *)iceCreamByName{
+    
+    NSMutableDictionary *iceCreamCount = [[NSMutableDictionary alloc] init];
+    
+    for(NSString *iceCreamFlavor in [iceCreamByName allValues]){
+        iceCreamCount[iceCreamFlavor] = @([[self namesForIceCream:iceCreamFlavor]count]); 
+    }
+    
+    NSLog(@"%@\n\n\n\n\n\n\n\n", iceCreamCount);
+    
+    return iceCreamCount;
+}
 
 @end
